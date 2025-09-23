f69411203ee8381a4219a45efdbdae496e0a962c
Javascript-Project <areeb9273@gmail.com>
2025-09-23 23:44:05 +0500

    updated with icons folder creation

---
+++ src/app/Icons/page.js
@@ -0,0 +1,9 @@
+import GoogleIcon from '@mui/icons-material/Google';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import AppleIcon from '@mui/icons-material/Apple';
+
+export const icons = {
+  googleIcon: GoogleIcon,
+  facebookIcon: FacebookIcon,
+  appleIcon: AppleIcon,
+};
